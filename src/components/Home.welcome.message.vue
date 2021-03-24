00ecf486011433ecb70f001430f5543317e6afd5
Vlado <vanovac.vlado@gmail.com>
2021-03-24 16:05:37 +0100

    greeting current user in welcome message

--- src/components/Home.welcome.message.vue
+++ src/components/Home.welcome.message.vue
@@ -2,7 +2,7 @@
   <v-container fill-height>
     <v-layout align-center>
       <v-flex>
-        <h1 class="display-1">Welcome back! We are glad to have you here.</h1>
+        <h1 class="display-1">Welcome back {{userData && userData.username}}! We are glad to have you here.</h1>
         <h3 class="display-0">Check below where were you leave off...</h3>
       </v-flex>
     </v-layout>
@@ -10,8 +10,13 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "home.welcome.message"
+  name: "home.welcome.message",
+  computed: {
+    ...mapGetters(['userData'])
+  }
 }
 </script>
 
